6088e53b8737d8ff4f16c2e776f483c1db7d6890
Thennam Pandian <thennam@gmail.com>
2018-04-18 00:38:24 +0530

    LCA is not working for binary tree, need to fix it

--- src/tree/LCA.java
+++ src/tree/LCA.java
@@ -5,8 +5,6 @@ import tree.util.TreeUtil;
 
 public class LCA {
 
-	private static boolean lcaFound = false;
-	
 	public static void main(String[] args) {
 		// Level 2 B tree
 		String[] values = { "1", "2", "3", "4", "5", "6", "7" };
@@ -15,32 +13,26 @@ public class LCA {
 		System.out.println(lca != null ? lca.getData() : "Not present");
 
 		// lca in left sub tree
-		lcaFound = false;
 		lca = lca(root, 4, 5);
 		System.out.println(lca != null ? lca.getData() : "Not present");
 
 		// lca in right sub tree
-		lcaFound = false;
 		lca = lca(root, 6, 7);
 		System.out.println(lca != null ? lca.getData() : "Not present");
 
 		// lca is not there
-		lcaFound = false;
 		lca = lca(root, 4, 17);
 		System.out.println(lca != null ? lca.getData() : "Not present");
 
 		// parent is lca
-		lcaFound = false;
 		lca = lca(root, 4, 1);
 		System.out.println(lca != null ? lca.getData() : "Not present");
 
 		// null tree
 		lca = lca(null, 4, 17);
-		lcaFound = false;
 		System.out.println(lca != null ? lca.getData() : "Not present");
 
 		// Single Element
-		lcaFound = false;
 		values = new String[] { "1" };
 		root = TreeUtil.createTree(values);
 		lca = lca(root, 4, 17);
@@ -48,26 +40,30 @@ public class LCA {
 
 		// left skews
 		System.out.println("\n");
-		lcaFound = false;
 		values = new String[] { "50", "30", "&", "20", "&", "&", "&" };
 		root = TreeUtil.createTree(values);
 		lca = lca(root, 30, 20);
 		System.out.println(lca != null ? lca.getData() : "Not present");
 
 		// Right skews
-		lcaFound = false;
 		System.out.println("\n");
 		values = new String[] { "50", "&", "170", "&", "&", "&", "180" };
 		root = TreeUtil.createTree(values);
 		lca = lca(root, 170, 180);
 		System.out.println(lca != null ? lca.getData() : "Not present");
-		
-		// tree with duplicate node
-		lcaFound = false;
+
+		// tree with duplicate node in left sub tree
 		values = new String[] { "1", "14", "3", "4", "7", "4", "17" };
 		root = TreeUtil.createTree(values);
 		lca = lca(root, 4, 7);
-		System.out.println(" duplicate ==> " + lca != null ? lca.getData() : "Not present");
+		System.out.println(" duplicate ==> " + lca != null ? lca.getData()
+				: "Not present");
+
+		// tree with duplicate node in right sub tree
+		values = new String[] { "1", "4", "3", "4", "17", "4", "7" };
+		root = TreeUtil.createTree(values);
+		lca = lca(root, 4, 7);
+		System.out.println(lca != null ? lca.getData() : "Not present");
 
 	}
 
@@ -88,12 +84,10 @@ public class LCA {
 			return root;
 
 		Tree left = findLCA(root.getLeft(), n1, n2);
-		Tree right = lcaFound == true ? null : findLCA(root.getRight(), n1, n2);
+		Tree right = findLCA(root.getRight(), n1, n2);
 
-		if (left != null && right != null) {
-			lcaFound = true;
+		if (left != null && right != null)
 			return root;
-		}
 		else if (left == null && right != null)
 			return right;
 		else if (left != null && right == null)
